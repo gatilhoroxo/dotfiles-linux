acea0dd98be4729cd19d5006da10c5c9c6781bfb
gatilhoroxo <gatilhoroxo@gmail.com>
2026-08-14 07:19:30 -0300

    test: mudando log.sh e tbm core.sh

--- portable-dev/lib/log.sh
+++ portable-dev/lib/log.sh
@@ -6,9 +6,34 @@
 # 
 # 
 
-log_info()
-log_success()
-log_warning()
-log_error()
-log_debug()
+log_info(){
+    #mensagem em azul
+    if [ -z $1 ]; then
+        echo "${blue}${g_black}## $1 ##${nc}"
+    fi
+}
+log_success(){
+    #mensagem em verde
+    if [ -z $1 ]; then
+        echo "${green}${g_black}## $1 ##${nc}"
+    fi
+}
+log_warning(){
+    #mensagem em amarelo
+    if [ -z $1 ]; then
+        echo "${yellow}${g_black}## $1 ##${nc}"
+    fi
+}
+log_error(){
+    #mensagem em vermelho
+    if [ -z $1 ]; then
+        echo "${red}${g_black}## $1 ##${nc}"
+    fi
+}
+log_debug(){
+    #mensagem em roxo
+    if [ -z $1 ]; then
+        echo "${purple}${g_black}## $1 ##${nc}"
+    fi
+}
 

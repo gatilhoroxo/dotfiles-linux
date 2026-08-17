91f45af3116b6d85a064358d20e879a7f4ae762d
gatilhoroxo <gatilhoroxo@gmail.com>
2026-08-17 14:29:56 -0300

    test: mudando command.sh

--- portable-dev/lib/command.sh
+++ portable-dev/lib/command.sh
@@ -6,8 +6,10 @@
 # Depende de ...
 # 
 
+# retorna caminho do comando?
 command_path() 
 
+#retorna se existe ou não
 command_exists() {
   local cmd="$1"
   [ -z "$cmd" ] && return 1
@@ -15,11 +17,22 @@ command_exists() {
   command -v "$cmd" > /dev/null 2>&1
 }
 
+# retorna versão do comando?
 command_version()
 
+#requer algum comando para continuar algo
+#vê se ele existe
+#se n existir, mostrar modo para instalação?
+#ou seguir para instalar ele?
 require_command()
 
+#requer vários comandos diferentes
+#vê se eles existem
+#se n existir, mostrar modo para instalação?
+#ou seguir para instalar eles?
+#vai depender do installer?
 require_commands()
 
+#não é a mesma coisa que o command_path?
 find_command()
 
